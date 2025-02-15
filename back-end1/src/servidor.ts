324027e99dcaac8e39748622c406d7270bd6d8d5
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-02-14 21:31:12 -0400

    server tb

--- back-end1/src/servidor.ts
+++ back-end1/src/servidor.ts
@@ -6,14 +6,16 @@ import RotasUsuário from "./rotas/rotas-usuário";
 import RotasMaestro from "./rotas/rotas-maestro";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
 app.use("/usuarios", RotasUsuário);
 app.use("/maestros", RotasMaestro);
-app.listen(PORT || 3333);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 const conexão = createConnection();
 
